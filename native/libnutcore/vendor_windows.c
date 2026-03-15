0f16e9e8e01ed9e4752588b3b639bc3ffd1de9e0
Pandelis <p@ndel.is>
2026-03-15 22:53:01 +0000

    core

--- native/libnutcore/vendor_windows.c
+++ native/libnutcore/vendor_windows.c
@@ -1,3 +1,19 @@
+#ifndef UNICODE
+#define UNICODE
+#endif
+
+#ifndef _UNICODE
+#define _UNICODE
+#endif
+
+#ifndef WINVER
+#define WINVER 0x0A00
+#endif
+
+#ifndef _WIN32_WINNT
+#define _WIN32_WINNT 0x0A00
+#endif
+
 #include "../../../libnut-core/src/win32/keycode.c"
 #include "../../../libnut-core/src/win32/keypress.c"
 #include "../../../libnut-core/src/win32/mouse.c"
